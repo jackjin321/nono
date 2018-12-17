e0039e4643e706f3fc4fe2e8786d928fec050b32
nono-office <catjuicy@gmail.com>
2018-12-17 19:19:57 +0800

    修改了缓存的方式

--- cache.go
+++ cache.go
@@ -6,20 +6,17 @@ import (
 )
 
 type cacheStruct struct {
+	id   int64
 	time int64
 	lock int32
 	date interface{}
+	//TODO 加入一个固定短时间,如果一直就不刷新
 }
 
 var cache = make(map[string]*cacheStruct)
 
-//CacheRead 仅仅读取返回值，如果
-func CacheRead(name string) interface{} {
-	return Cache(name, 0, nil)
-}
-
-//Cache 输入名字,过期时间(秒), 需要运行的函数,如果超时,那么运行函数并把返回值缓存到名字里
-func Cache(name string, seccnd int64, f func() interface{}) interface{} {
+//Cache 输入名字,过期时间(秒),一致的id,如果 id相同,那么即使超时了也不重新读取 需要运行的函数,如果超时,那么运行函数并把返回值缓存到名字里
+func Cache(name string, seccnd int64, f func(id int64) (interface{}, int64)) interface{} {
 	c := cache[name]
 	if c == nil {
 		c = &cacheStruct{}
@@ -28,10 +25,20 @@ func Cache(name string, seccnd int64, f func() interface{}) interface{} {
 	if f == nil {
 		return c.date
 	}
+	//如果id相等,直接返回
+
 	if time.Now().Unix()-c.time > seccnd {
 		if i := atomic.AddInt32(&c.lock, 1); i == 1 {
+			result, idd := f(c.id)
+			if idd != 0 && idd == c.id {
+				c.time = time.Now().Unix()
+			}
+			if result == nil {
+				return c.date
+			}
 			c.time = time.Now().Unix()
-			c.date = f()
+			c.date = result
+			c.id = idd
 			atomic.StoreInt32(&c.lock, 0)
 			return c.date
 		}
